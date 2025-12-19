8998a0dddbe76eb327a13f82ac197a77f2515251
dejoyasferrando <69395434+dejoyasferrando@users.noreply.github.com>
2025-12-19 10:16:53 -0300

    Primera versión del sitio Surgentes 2025

---
+++ funciones.js
@@ -0,0 +1,35 @@
+const tituloTexto = "SURGENTES";
+const textoTexto = " Colectivo artístico que trabaja en el cruce entre territorio, procesos materiales y tecnologías inestables. Su práctica se centra en la experimentación, la falla y el error como formas de conocimiento, abordando lo visible y lo oculto en los sistemas naturales, sociales y técnicos. El trabajo del colectivo se desarrolla a través de instalaciones, esculturas sonoras,  dispositivos experimentales y registros audiovisuales, poniendo en tensión las ideas de control,  eficiencia y progreso.";
+     
+     
+
+let i = 0;
+let j = 0;
+
+const titulo = document.getElementById("titulo");
+const texto = document.getElementById("texto");
+
+function escribirTitulo() {
+  if (i < tituloTexto.length) {
+    titulo.textContent += tituloTexto[i];
+    i++;
+    setTimeout(escribirTitulo, 120);
+  } else {
+    setTimeout(escribirTexto, 500);
+  }
+}
+
+function escribirTexto() {
+  if (j < textoTexto.length) {
+    texto.textContent += textoTexto[j];
+    j++;
+    setTimeout(escribirTexto, 40);
+  }
+}
+
+setTimeout(() => {
+  document.querySelector(".menu").style.opacity = 1;
+}, 27000);
+
+
+escribirTitulo();
