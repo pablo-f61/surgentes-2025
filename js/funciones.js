96804f6e73d6be76f9d709eb4d845b20a075d8e4
dejoyasferrando <69395434+dejoyasferrando@users.noreply.github.com>
2025-12-28 19:38:26 -0300

    Agrego portal desde HTML CSS JS

--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,6 @@
+
+
+
 const tituloTexto = "SURGENTES";
 const textoTexto = " Colectivo artístico que trabaja en el cruce entre territorio, procesos materiales y tecnologías inestables. Su práctica se centra en la experimentación, la falla y el error como formas de conocimiento, abordando lo visible y lo oculto en los sistemas naturales, sociales y técnicos. El trabajo del colectivo se desarrolla a través de instalaciones, esculturas sonoras,  dispositivos experimentales y registros audiovisuales, poniendo en tensión las ideas de control,  eficiencia y progreso.";
      
@@ -6,18 +9,47 @@ const textoTexto = " Colectivo artístico que trabaja en el cruce entre territor
 let i = 0;
 let j = 0;
 
+function iniciarTexto() {
+  i = 0;
+  j = 0;
+  titulo.textContent = "";
+  texto.textContent = "";
+  escribirTitulo();
+}
+
+const portal = document.getElementById("portal");
+const main = document.querySelector("main");
+
 const titulo = document.getElementById("titulo");
 const texto = document.getElementById("texto");
 
 const audio = document.getElementById("maquina");
 let activado = false;
 
-window.addEventListener("click", () => {
-  if (!activado) {
-    audio.play();
-    activado = true;
-  }
+  portal.addEventListener("click", () => {
+  portal.style.opacity = 0;
+  portal.style.pointerEvents = "none";
+
+  main.style.opacity = 1;
+  main.style.pointerEvents = "auto";
+
+  audio.currentTime = 0;
+  audio.play();
+
+  // acá llamás a la función que inicia el tipeo
+  iniciarTexto();
+
+setTimeout(() => {
+  document.querySelector(".menu").style.opacity = 1;
+}, 26000);
+
+setTimeout(() => {
+  audio.pause();
+  audio.currentTime = 0;
+}, 26000);
+
 });
+
 
 function escribirTitulo() {
   if (i < tituloTexto.length) {
@@ -37,14 +69,7 @@ function escribirTexto() {
   }
 }
 
-setTimeout(() => {
-  document.querySelector(".menu").style.opacity = 1;
-}, 25500);
 
 
-setTimeout(() => {
-  audio.pause();
-  audio.currentTime = 0;
-}, 25500);
 
-escribirTitulo();
+
