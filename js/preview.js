dcf22cfb82a7539464e24216803aa573cc7dd7a0
dejoyasferrando <69395434+dejoyasferrando@users.noreply.github.com>
2026-03-21 20:18:18 -0300

    Lupupa funcionando

--- js/preview.js
+++ js/preview.js
@@ -4,13 +4,16 @@ const preview = document.getElementById("preview");
 links.forEach(link => {
   link.addEventListener("mouseenter", (e) => {
     const img = link.getAttribute("data-img");
-
     if (img) {
       preview.style.backgroundImage = `url(${img})`;
       preview.style.opacity = '1';
     }
   });
 
+  link.addEventListener("mousemove", (e) => {
+    preview.style.left = e.clientX + "px";
+    preview.style.top = e.clientY + "px";
+  });
 
   link.addEventListener("mouseleave", () => {
     preview.style.opacity = '0';
