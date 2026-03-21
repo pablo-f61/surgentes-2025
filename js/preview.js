0672282372af3e8f8deb7949ca60f969695bd5f0
dejoyasferrando <69395434+dejoyasferrando@users.noreply.github.com>
2026-03-21 20:06:23 -0300

    Lupa previwew

---
+++ js/preview.js
@@ -0,0 +1,18 @@
+const links = document.querySelectorAll(".menu a");
+const preview = document.getElementById("preview");
+
+links.forEach(link => {
+  link.addEventListener("mouseenter", (e) => {
+    const img = link.getAttribute("data-img");
+
+    if (img) {
+      preview.style.backgroundImage = `url(${img})`;
+      preview.style.opacity = '1';
+    }
+  });
+
+
+  link.addEventListener("mouseleave", () => {
+    preview.style.opacity = '0';
+  });
+});
